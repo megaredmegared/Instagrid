7fd8c7f529f46d0476a111f50e1d726bd744246b
megaredmegared <sarseb@gmail.com>
2018-11-22 10:04:05 +0100

    Project creation and first steps layout

---
+++ Instagrid/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by megared on 21/11/2018.
+//  Copyright © 2018 OpenClassrooms. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+    
+
+
+}
+
